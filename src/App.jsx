411970584c6c7411b1bda5c26ffe8f87a4c1e7fe
YUNA9627 <yuna2175@naver.com>
2026-04-10 23:19:02 +0900

    feat: React Router 기반 페이지 라우팅 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Route } from "react-router-dom";
+import "./App.css";
+import Applayout from "./layout/Applayout";
+import Homepage from "./pages/Homepage/Homepage";
+import MoviePage from "./pages/Movies/MoviePage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
+// 홈페이지 /
+// 영화 전체 보여주는 페이지 (서치) /movies
+// 영화 디테일 페이지 /movies/:id
+// 추천영화 /movies/:id/recommandation
+// 리뷰 /movies/:id/reviews
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Applayout />}>  // user화면
+        <Route index element={<Homepage/>}/>
+        <Route path="movies">
+          <Route index element={<MoviePage/>}/>
+          <Route path=":id" element={<MovieDetailPage/>}/>
+        </Route>
+        {/* <Route path="/movies" element={<MoviePage/>}/>
+        <Route path="/movies/:id" element={<MovieDetailPage/>}/> */}
+      </Route>
+      <Route path="*" element={<NotFoundPage/>}/>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
