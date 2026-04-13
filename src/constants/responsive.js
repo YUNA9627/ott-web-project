6d12bae480ab24e7a1df4783cd4574bec6389f4d
YUNA9627 <yuna2175@naver.com>
2026-04-13 21:48:07 +0900

    refactor: 공통 슬라이드 responsive 설정 추가

---
+++ src/constants/responsive.js
@@ -0,0 +1,26 @@
+export const responsive = {
+  superdesktop: {
+    breakpoint: { max: 3000, min: 1400 },
+    items: 8,
+  },
+  desktop: {
+    breakpoint: { max: 1400, min: 1280 },
+    items: 6,
+  },
+  laptop: {
+    breakpoint: { max: 1280, min: 1024 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 600 },
+    items: 3,
+  },
+  tablet2: {
+    breakpoint: { max: 600, min: 360 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 360, min: 0 },
+    items: 1,
+  },
+};
