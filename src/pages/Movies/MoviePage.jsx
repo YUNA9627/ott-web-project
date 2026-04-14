06886e988ac17465749ee1f0ae454a850edb46ee
YUNA9627 <yuna2175@naver.com>
2026-04-14 23:41:55 +0900

    feat: 영화 목록 페이지 검색 및 페이지네이션 구현

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactPaginateModule from "react-paginate";
+import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
+import { useSearchParams } from "react-router-dom";
+import { Alert, Col, Container, Row } from "react-bootstrap";
+import MovieCard from "../../common/MovieCard/MovieCard.jsx";
 
+const ReactPaginate = ReactPaginateModule.default ?? ReactPaginateModule;
+
+// 경로 2가지
+// - navBar에서 클릭하면서 온 경우 => popularMovie 보여주기
+// - keyWord를 입력해서 온 경우 => keyword와 관련된 영화들을 보여줌
+
+// 페이지네이션 설치
+// page state 만들기
+// 페이지네이션 클릭할 때마다 page 바꿔주기
+// page 값이 바뀔 때 마다 useSearchMovie에 page까지 넣어서 fetch
 const MoviePage = () => {
-  return <div>MoviePage</div>;
+  const [query] = useSearchParams();
+  const [page, setPage] = useState(1);
+  const keyword = query.get("q");
+
+  const { data, isLoading, isError, error } = useSearchMovieQuery({
+    keyword,
+    page,
+  });
+
+  const handlePageClick = ({ selected }) => {
+    setPage(selected + 1);
+  };
+
+  if (isLoading) {
+    return <h1>Loading..</h1>;
+  }
+
+  if (isError) {
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col lg={4} xs={12}>
+          필터
+        </Col>
+        <Col lg={8} xs={12}>
+          <Row>
+            {data?.results.map((movie, index) => (
+              <Col key={index}>
+                <MovieCard movie={movie} />
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+      <ReactPaginate
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageCount={data?.total_pages} // 전체 페이지가 몇 개인지
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+        forcePage={page - 1}
+      />
+    </Container>
+  );
 };
 
 export default MoviePage;
