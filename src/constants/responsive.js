f5d0f88a035158f7c45c7af51faed1939eef2115
YUNA9627 <yuna2175@naver.com>
2026-04-18 01:35:03 +0900

    feat: 반응형에 따른 슬라이드 아이템 갯수 조절

--- src/constants/responsive.js
+++ src/constants/responsive.js
@@ -5,22 +5,22 @@ export const responsive = {
   },
   desktop: {
     breakpoint: { max: 1400, min: 1280 },
-    items: 6,
+    items: 8,
   },
   laptop: {
     breakpoint: { max: 1280, min: 1024 },
-    items: 4,
+    items: 6,
   },
   tablet: {
     breakpoint: { max: 1024, min: 600 },
-    items: 3,
+    items: 4,
   },
   tablet2: {
     breakpoint: { max: 600, min: 360 },
-    items: 2,
+    items: 3,
   },
   mobile: {
     breakpoint: { max: 360, min: 0 },
-    items: 1,
+    items: 2,
   },
 };
